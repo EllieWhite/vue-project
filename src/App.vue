0ad30ed429b3f20edb08bbd2fb20672003ac37c6
Ellie <diana.lyutaya12@gmail.com>
2025-10-16 21:43:43 +0300

    добавлено поле ввода

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,63 @@
+<template>
+  <div class="container todo-app">
+    <h1 class="title">Todo List</h1>
+
+
+    <TodoForm @add-to="handleAddTo" />
+    <TodoList :todos="todos" @remove-todo="removeTodo" />
+
+    <TodoFooter
+      v-if="todos.length"
+      :remaining="remainingTodos"
+      @clear-completed="clearCompleted"
+      @clear-all="clearAll"
+    />
+  </div>
+</template>
+
 <script setup>
+import { reactive, computed, ref } from 'vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoForm from './components/TodoForm.vue'
 
-  import { ref, reactive, computed } from 'vue';
-  import TodoItem from './TodoItem/TodoItem.vue';
+const todos = reactive([
+  { id: 1, text: 'Изучить компоненты Vue.js', completed: true },
+  { id: 2, text: 'Создать TodoList приложение', completed: false },
+  { id: 3, text: 'Похвалить себя за отличную работу', completed: false },
+])
 
-  const todos = reactive([
-    {
-      id: 1,
-      text: 'Изучить компоненты Vue.js',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Создать TodoList приложение',
-      completed: false
-    },
-    {
-      id: 3,
-      text: 'Похвалить себя за отличную работу',
-      completed: false
-    },
-  ])
+const removeTodo = (index) => {
+  todos.splice(index, 1)
+}
 
-  const removeTodo = (index) => {
-    todos.splice(index, 1);
-    return(todos.length)
-  }
-  const todosEmpty = computed(() => todos.length === 0);
+const remainingTodos = computed(() =>
+  todos.filter((todo) => !todo.completed).length
+)
 
-  const remainingTodos = computed(() => {
-    return todos.filter((todo) => !todo.completed).length
-  })
-
-  const clearCompleted  = () => {
-    for (let i = 0; i < todos.length; i++) {
-      if(todos[i].completed) {
-        todos.splice(i, 1)
-      }
+const clearCompleted = () => {
+  for (let i = todos.length - 1; i >= 0; i--) {
+    if (todos[i].completed) {
+      todos.splice(i, 1)
     }
   }
+}
 
-  const clearAll  = () => {
-        todos.splice(0, todos.length)
+const clearAll = () => {
+  todos.splice(0, todos.length)
+}
+
+  const handleAddTo = (task) => {
+    const newTodo = {
+      id: Date.now(),
+      text: task,
+      completed: false
+    }
+    todos.push(newTodo)
+
+    console.log(newTodo.id)
   }
+
 </script>
-
-<template>
-      <div class="container todo-app">
-      <h1 class="title">Todo List</h1>
-      <div class="todo-app__main">
-        <ul class="todo-list">
-          <li  v-for="(todo, index) in todos" :key="todo.id" :class="{ 'todo-list__item--completed': todo.completed }" class="todo-list__item">
-            <TodoItem 
-              v-bind="todo" 
-              @complete-todo.once="() => (todo.completed = true)" 
-              @remove-todo="removeTodo(index)" 
-            />
-          </li>
-        </ul>
-        <div v-if="todosEmpty" class="todo-list__empty">
-          <p>Список задач пуст</p>
-        </div>
-      </div>
-
-      <div class="todo-app__footer" v-if="!todosEmpty">
-        <p class="todo-app__footer-text">Осталось {{ remainingTodos }} задания(й)</p>
-        <button class="btn btn--clear" @click="clearCompleted">Удалить завершенные</button>
-        <button class="btn btn--clear" @click="clearAll">Очистить список</button>
-      </div>
-    </div>
-</template>
 
 <style src="./App.css"></style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref } from 'vue';
+    const taskValue = ref('')
+    const emit = defineEmits(['add-to'])
+
+    const addTo = () => {
+        if(taskValue.value !== '') {
+            emit('add-to', taskValue.value);
+            taskValue.value = ''
+        }
+    }
+</script>
+
+<template>
+    <form class="form">
+      <div class="form__group form__group--todo">
+        <input v-model.trim="taskValue" class="form-control" type="text" placeholder="Новая задача" />
+        <button class="btn btn--add-todo" type="submit" @click.stop.prevent="addTo">Добавить</button>
+      </div>
+    </form>
+</template>
