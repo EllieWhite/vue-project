da5730e53cf4ce1af55498f0e35d664e37c1a5fe
Ellie <diana.lyutaya12@gmail.com>
2025-12-04 16:07:33 +0300

    добавлены новые темы

--- src/store/counter.js
+++ src/store/counter.js
@@ -1,21 +1,21 @@
-// import { acceptHMRUpdate, defineStore } from "pinia";
-// import { ref, computed } from "vue";
+import { acceptHMRUpdate, defineStore } from "pinia";
+import { ref, computed } from "vue";
 
 
-// export const useCounterStore = defineStore("counter", () => {
-//     const counter = ref(0)
+export const useCounterStore = defineStore("counter", () => {
+    const counter = ref(0)
 
-//     const doubleCount = computed(() => counter.value * 2);
+    const doubleCount = computed(() => counter.value * 2);
 
-//     const increment = () => {
-//         counter.value++
-//     }
+    const increment = () => {
+        counter.value++
+    }
 
-//     return {
-//         counter, doubleCount, increment
-//     }
-// })
+    return {
+        counter, doubleCount, increment
+    }
+})
 
-// if (import.meta.hot) {
-//     import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot));
-// }
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot));
+}
