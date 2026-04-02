82dd0d001a159dc983d290284f4f4f5d210b1e07
Ellie <diana.lyutaya12@gmail.com>
2026-04-03 00:09:12 +0300

    регистрация и пользователь

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import register from './controllers/user'
 
 const port = 3001;
 const app = express();
 
 app.use(cookieParser())
 app.use(express.json())
+
+app.post('/registrater', async (req, res) => {
+    try {
+        const user = await register(req.body.login)
+        req.send({error: null, user: mapUser(user)})
+    } catch (e) {
+        res.send({error: e.message || 'Unknown error'})
+    }
+})
 
 mongoose.connect(
     'mongodb+srv://dianalyutaya12_db_user:6zv&pQgS3GqV@cluster0.tckqa1m.mongodb.net/notes?appName=Cluster0'
